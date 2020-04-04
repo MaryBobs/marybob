edca135188f329fc07f346cc63e3dcc929e0a119
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-04 15:52:23 +0100

    initial set up

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => (
+    <header className="app-header">
+    <ul className="container">
+        <li>Main Page</li>
+    </ul>
+</header>
+);
+
+export default Header;
