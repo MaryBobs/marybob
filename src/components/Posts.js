099f2c0f46a51a841e2607a6bc1ba409ef41fd24
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-04 16:28:32 +0100

    adds dummy links

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Posts = ({posts}) => (
     <article className="posts container">
@@ -8,7 +9,7 @@ const Posts = ({posts}) => (
             )}
             {posts.map(post => (
                 <li key={post.id}>
-                    <h2>{post.title}</h2>
+                    <h2><Link to={`/post/${post.slug}`}>{post.title}</Link></h2>
                 </li>
             ))}
         </ul>
