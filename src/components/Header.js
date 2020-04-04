78e83b120ce6941e9746f701c13a04402d07df17
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-04 16:30:55 +0100

    adds link to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = props => (
     <header className="app-header">
     <ul className="container">
-        <li>Main Page</li>
+        <li key="home"><Link to="/">Home Page</Link></li>
     </ul>
 </header>
 );
