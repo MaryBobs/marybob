7d9071b13330be124f0636a5f24a451f99d38e09
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-04 16:07:47 +0100

    adds Posts and checks dummy data displays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Posts from './components/Posts';
 
 class App extends Component {
+  state = {
+    posts: [
+      {id: 1,
+      title: "1st blog post",
+      content: "welcome to my blog"},
+      {id: 2,
+      title: "Me again!",
+      content: "todays project"},
+      {id: 3,
+      title: "next blog",
+      content: "look at what I made today!"}
+    ]
+  }
   render() {
     return (
       <div className="app">
         MARYBOBS
         <Header />
+        <Posts posts={this.state.posts}></Posts>
       </div>
     );
   }
