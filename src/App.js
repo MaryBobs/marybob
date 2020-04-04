c63a37bc925a6bb6eeead61c2852c6fa1d40cb2a
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-04 16:25:37 +0100

    adds react router

--- src/App.js
+++ src/App.js
@@ -2,28 +2,38 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Posts from './components/Posts';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends Component {
   state = {
     posts: [
       {id: 1,
+      slug: "hello-post",
       title: "1st blog post",
       content: "welcome to my blog"},
       {id: 2,
+      slug: "hello-again",
       title: "Me again!",
       content: "todays project"},
       {id: 3,
+      slug: "hello-blog",
       title: "next blog",
       content: "look at what I made today!"}
     ]
   }
   render() {
     return (
+      <Router>
       <div className="app">
         MARYBOBS
         <Header />
-        <Posts posts={this.state.posts}></Posts>
+        <Switch>
+          <Route exact path="/"
+          render={() => <Posts posts={this.state.posts} />}
+          />
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
