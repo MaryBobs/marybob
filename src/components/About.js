0916201eb439432f0fdde46d3681205ed3cf3afe
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-14 11:51:21 +0100

    adds dummy about text

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => (
-    <h2>About</h2>
-)
+    <div className="info-box">
+            <p>Hello, I'm Mary</p>
+            <p>I live in the beautiful city of Edinburgh here in sunny Scotland.</p>
+            <p>I recently left my full time job to embark on a new career and started by completing the Professional Software Development course at CodeClan</p>
+            <p>I am now looking for my first role as a junior software developer</p>
+    </div>
+    )
 
 export default About;
