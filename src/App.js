629dc932bae33702883cbbb9217903297637e9a2
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-14 10:00:32 +0100

    moves and renames blog files

--- src/App.js
+++ src/App.js
@@ -1,144 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
+// import HomePageContainer from './containers/HomePageContainer';
+import Blog from './containers/Blog';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Posts from './components/Posts';
-import Post from './components/Post';
-import NotFound from './components/NotFound';
-import PostForm from './components/PostForm';
-import Message from './components/Message.js';
-import SimpleStorage from 'react-simple-storage';
-import Login from './components/Login';
-import firebase from './firebase';
 
-class App extends Component {
-  state = {
-    posts: [],
-    message: null,
-    isAuthenticated: false
-  };
-
-  componentDidMount() {
-    const postsRef = firebase.database().ref("posts");
-    postsRef.on("value", snapshot => {
-      const posts = snapshot.val();
-      const newStatePosts = [];
-      for (let post in posts) {
-        newStatePosts.push({
-          key: post,
-          slug: posts[post].slug,
-          title: posts[post].title,
-          content: posts[post].content
-        });
-      }
-      this.setState({ posts: newStatePosts });
-    });
-  }
-
-  getNewSlugFromTitle = (title) => 
-    encodeURIComponent(title.toLowerCase().split(" ").join("-")
+function App() {
+  return (
+    <div className="App">
+     {/* <HomePageContainer></HomePageContainer> */}
+     <Blog>Blog</Blog>
+    </div>
   );
-  
-  addNewPost = (post) => {
-    const postsRef = firebase.database().ref("posts");
-    post.slug = this.getNewSlugFromTitle(post.title);
-    delete post.key;
-    postsRef.push(post);
-    this.setState({
-      message: "saved"
-    });
-    setTimeout(() => {
-      this.setState({ message: null})
-    }, 1600);
-  };
-
-  updatePost = (post) => {
-    const postRef = firebase.database().ref("posts/" + post.key);
-    postRef.update({
-      slug: this.getNewSlugFromTitle(post.title),
-      title: post.title,
-      content: post.content
-    });
-    this.setState({ message: "updated" });
-    setTimeout(() => {
-      this.setState({ message: null})
-    }, 1600);
-  };
-
-  deletePost = (post) => {
-    if (window.confirm(`Are you sure you want to Delete "${post.title}"?`)) {
-      const postRef = firebase.database().ref("posts/" + post.key);
-      postRef.remove();
-      this.setState({ message: "deleted" });
-      setTimeout(() => {
-        this.setState({ message: null });
-      }, 1600);
-    }
-  };
-
-  onLogin = (email, password) => {
-    firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(user => this.setState({ isAuthenticated: true }))
-    .catch(error => console.error(error));
-  }
-
-  onLogout = () => {
-    firebase
-    .auth()
-    .signOut()
-    .then( () => {
-      this.setState({ isAuthenticated: false });
-          })
-      .catch(error => console.error(error));
-    };
-
-  render() {
-    return (
-      <Router>
-      <div className="app">
-        MARYBOBS
-        <SimpleStorage parent={this} />
-        <Header isAuthenticated={this.state.isAuthenticated} onLogout={this.onLogout} />
-        {this.state.message && <Message type={this.state.message} />}
-        <Switch>
-          <Route exact path="/"
-          render={() => <Posts posts={this.state.posts} deletePost={this.deletePost} isAuthenticated={this.state.isAuthenticated}/>}
-          />
-          <Route path="/post/:postSlug"
-          render={props => {
-            const post = this.state.posts.find(post => post.slug === props.match.params.postSlug);
-            if (post)
-            return <Post post={post} />;
-            else return <NotFound />;
-          }}
-          />
-          <Route exact path="/login" render={() => !this.state.isAuthenticated ? (<Login onLogin={this.onLogin} />) : (
-            <Redirect to="/" /> 
-          ) } />
-          <Route exact path="/new" render={() => this.state.isAuthenticated ? (
-            <PostForm addNewPost={this.addNewPost} 
-            post={{ key: null, slug: "", title: "", content: "" }} />) : (
-              <Redirect to="/login" /> 
-              ) } />
-          <Route path="/edit/:postSlug" render={props => { 
-            const post =this.state.posts.find(
-              post => post.slug === props.match.params.postSlug
-            );
-            if (post && this.state.isAuthenticated) {
-              return <PostForm post={post} updatePost={this.updatePost} />;
-            } else if (post && !this.state.isAuthenticated) {
-              return <Redirect to="/login" />;
-            } else {
-              return <Redirect to="/" />;
-            }
-          }} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-      </Router>
-    );
-  }
 }
+
 export default App;
--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Header = props => (
+const BlogHeader = props => (
     <header className="app-header">
     <ul className="container">
         <li key="home"><Link to="/">Home Page</Link></li>
@@ -17,4 +17,4 @@ const Header = props => (
 </header>
 );
 
-export default Header;
+export default BlogHeader;
